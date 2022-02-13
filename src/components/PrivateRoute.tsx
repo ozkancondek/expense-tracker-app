4067fa970a9fa25d975ee7ada8ebc99465e9af58
ozkancondek <ozkncndek@gmail.com>
2022-02-13 22:51:23 +0100

    Lesson-7:Delete private route

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,15 +1,16 @@
-import { Navigate, Route, RouteProps } from "react-router-dom";
+export {};
+// import { Navigate, Route, RouteProps } from "react-router-dom";
 
-interface PrivateRouteProps extends RouteProps {}
+// interface PrivateRouteProps extends RouteProps {}
 
-function PrivateRoute({ element: Component }: PrivateRouteProps) {
-  // const token  = localStorage.getItem("token");
-  const token = false;
-  if (!token) {
-    console.log("ozkan");
-    return <Route element={<Navigate to="/login" />} />;
-  }
-  return <Route element={<Navigate to="/login" />} />;
-}
+// function PrivateRoute({ element: Component }: PrivateRouteProps) {
+//   // const token  = localStorage.getItem("token");
+//   const token = false;
+//   if (!token) {
+//     console.log("ozkan");
+//     return <Route element={<Navigate to="/login" />} />;
+//   }
+//   return <Route element={<Navigate to="/login" />} />;
+// }
 
-export default PrivateRoute;
+// export default PrivateRoute;
