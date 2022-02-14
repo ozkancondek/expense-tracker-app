73b560bb17662db5f81a4e48a3844e4e61688ed2
ozkancondek <ozkncndek@gmail.com>
2022-02-14 22:00:50 +0100

    Lesson-8-Category button edit delete functions

--- src/store/actions/categoryActions.ts
+++ src/store/actions/categoryActions.ts
@@ -23,19 +23,22 @@ export const addCategory =
   };
 
 export const updateCategory =
-  (form: Partial<CategoryForm>, categoryId: number) =>
-  async (dispatch: CategoryDispatch) => {
-    dispatch({ type: "UPDATE_CATEGORY_START" });
-    try {
-      const response = await api().put<Category>(
-        "/categories/" + categoryId,
-        form
-      );
-      dispatch({ type: "UPDATE_CATEGORY_SUCCESS", payload: response.data });
-    } catch {
-      dispatch({ type: "UPDATE_CATEGORY_ERROR" });
-    }
-  };
+  // i am gonna send only name prop to put request  thats why i am using Partial
+
+
+    (form: Partial<CategoryForm>, categoryId: number) =>
+    async (dispatch: CategoryDispatch) => {
+      dispatch({ type: "UPDATE_CATEGORY_START" });
+      try {
+        const response = await api().put<Category>(
+          "/categories/" + categoryId,
+          form
+        );
+        dispatch({ type: "UPDATE_CATEGORY_SUCCESS", payload: response.data });
+      } catch {
+        dispatch({ type: "UPDATE_CATEGORY_ERROR" });
+      }
+    };
 
 export const deleteCategory =
   (categoryId: number) => async (dispatch: CategoryDispatch) => {
